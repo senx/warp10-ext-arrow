5d2f14d4aa4b2e146938e15ca00cbaac8d4b81f5
jc <jean-charles.vialatte@senx.io>
2019-09-17 17:05:39 +0200

    update doc

--- src/test/java/arrow/GenerateDocumentation.java
+++ src/test/java/arrow/GenerateDocumentation.java
@@ -44,6 +44,8 @@ public class GenerateDocumentation extends RunAndGenerateDocumentationWithUnitTe
     return true;
   }
 
+  protected String VERSION() {return "2.2";}
+
   protected List<String> TAGS() {
     List<String> tags = new ArrayList<>();
     tags.add("arrow");
