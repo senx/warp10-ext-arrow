8b63c43b0cc326464c3aec695501c5463bdb2b42
Jean-Charles Vialatte <randomboolean@gmail.com>
2020-10-30 19:01:12 +0100

    prepare refactor TOARROW and ARROWTO

---
+++ src/main/java/io/warp10/arrow/convert/Converter.java
@@ -0,0 +1,39 @@
+//
+//   Copyright 2020  SenX S.A.S.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package io.warp10.arrow.convert;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface Converter<T> {
+
+    String getWarpScriptType();
+
+    boolean isConvertible(Object o);
+
+    void write(T object, OutputStream out);
+
+    T read(InputStream in);
+
+    /**
+     * Used if need to choose between converters of same type T.
+     * @return the level
+     */
+    default int getPriorityLevel(){
+        return 0;
+    }
+}
---
+++ src/main/java/io/warp10/arrow/convert/Register.java
@@ -0,0 +1,131 @@
+//
+//   Copyright 2020  SenX S.A.S.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package io.warp10.arrow.convert;
+
+import io.warp10.continuum.gts.GeoTimeSerie;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Register {
+
+    private final static Map<String, Converter> REGISTER = new HashMap<String, Converter>();
+
+    public static void addConverter(Converter converter){
+        if (!REGISTER.containsKey(converter.getWarpScriptType()) || converter.getPriorityLevel() <= REGISTER.get(converter.getWarpScriptType()).getPriorityLevel()) {
+            REGISTER.put(converter.getWarpScriptType(), converter);
+        }
+    }
+
+    public static Converter getConverter(String type) {
+      return REGISTER.get(type);
+    }
+
+    public static boolean hasConverterForType(String type) {
+        return REGISTER.containsKey(type);
+    }
+
+    static {
+
+        //
+        // ENCODERS
+        //
+
+        addConverter(new Converter<List>() {
+            @Override
+            public String getWarpScriptType() {
+                return "ENCODERS";
+            }
+
+            @Override
+            public boolean isConvertible(Object o) {
+                //TODO
+                return false;
+            }
+
+            @Override
+            public void write(List object, OutputStream out) {
+                //TODO
+            }
+
+            @Override
+            public List read(InputStream in) {
+                //TODO
+                return null;
+            }
+        });
+
+        //
+        // GTS
+        //
+
+        addConverter(new Converter<GeoTimeSerie>() {
+            @Override
+            public String getWarpScriptType() {
+                return "GTS";
+            }
+
+            @Override
+            public boolean isConvertible(Object o) {
+                //TODO
+                return false;
+            }
+
+            @Override
+            public void write(GeoTimeSerie gts, OutputStream out) {
+                //TODO
+            }
+
+            @Override
+            public GeoTimeSerie read(InputStream in) {
+                //TODO
+                return null;
+            }
+        });
+
+        //
+        // PAIR
+        //
+
+        addConverter(new Converter<List>() {
+            @Override
+            public String getWarpScriptType() {
+                return "PAIR";
+            }
+
+            @Override
+            public boolean isConvertible(Object o) {
+                //TODO
+                return false;
+            }
+
+            @Override
+            public void write(List object, OutputStream out) {
+                //TODO
+            }
+
+            @Override
+            public List read(InputStream in) {
+                //TODO
+                return null;
+            }
+        });
+    }
+}
